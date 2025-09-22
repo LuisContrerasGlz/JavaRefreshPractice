94f41b11f6f7b384f0a09cc613b2d2c48ede01a5
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-09-22 14:56:54 -0600

    Variables and constants

---
+++ VariablesAndConstants.java
@@ -0,0 +1,30 @@
+public class VariablesAndConstants {
+
+    public static void main(String[] args) {
+
+        // Variables
+
+        String name = "Luis";
+        System.out.println(name);
+
+        name = "Francisco";
+        System.out.println(name);
+
+        // name = 37; Error (no podemos cambiar el tipo de dato)
+
+        int age = 30;
+        System.out.println(age);
+
+        var email = "contrerasgonzalezluisfco@gmail.com";
+        System.out.println(email);
+
+        var year = 2025;
+        System.out.println(year);
+
+        // Constantes
+
+        final String EMAIL = "contrerasgonzalezluisfco@gmail.com";
+        // EMAIL = "contrerasgonzalezluisfco@gmail.com"; 
+        System.out.println(EMAIL);
+    }
+}
