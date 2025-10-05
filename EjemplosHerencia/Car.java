21cef7523e7f21cae124c6eaebcad84309686882
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-04 20:57:24 -0600

    Herencia ejercicios

---
+++ EjemplosHerencia/Car.java
@@ -0,0 +1,11 @@
+package EjemplosHerencia;
+
+class Car extends Vehicle {
+    public void honk() {
+        System.out.println("Car: Beep beep!");
+    }
+    @Override
+    public String describe() {
+        return "Car: 4 wheels, comfortable for roads.";
+    }
+}
