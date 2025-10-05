bff08a37f09a5eef87b301ffdae7a9d20926d5f4
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-04 20:23:45 -0600

    modificadores

---
+++ ProgramacionOrientadaObjetos2/Car.java
@@ -0,0 +1,21 @@
+package ProgramacionOrientadaObjetos2;
+
+public class Car {
+    private int speed;
+
+    public int getSpeed() { return speed; }
+
+    public void accelerate(int amount) {
+        if (amount > 0) {
+            speed += amount;
+            if (speed > 120) speed = 120;
+        }
+    }
+
+    public void brake(int amount) {
+        if (amount > 0) {
+            speed -= amount;
+            if (speed < 0) speed = 0;
+        }
+    }
+}
