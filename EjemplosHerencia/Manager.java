dd1572c89e42248432e627bec1aab03e5ab561ae
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-04 21:00:57 -0600

    Herencia ejercicios

---
+++ EjemplosHerencia/Manager.java
@@ -0,0 +1,15 @@
+package EjemplosHerencia;
+
+class Manager extends Employee {
+    private String department;
+
+    public Manager(String name, double salary, String department) {
+        super(name, salary);
+        this.department = department;
+    }
+
+    @Override
+    public String describe() {
+        return "Manager " + name + " (Dept: " + department + ") earns " + salary;
+    }
+}
