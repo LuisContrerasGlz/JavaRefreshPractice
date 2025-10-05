b3c6a73d0f7b04dafccfadc367fdabd46dc14659
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-04 20:57:01 -0600

    Herencia ejercicios

---
+++ EjemplosHerencia/Vehicle.java
@@ -0,0 +1,10 @@
+package EjemplosHerencia;
+
+class Vehicle {
+    public void move() {
+        System.out.println("Vehicle is moving...");
+    }
+    public String describe() { // para el punto 9
+        return "Generic vehicle";
+    }
+}
